d0829466c26cae0d5200278f3b16c7dbcfdeae57
Angel Yankov <angy@bang-olufsen.dk>
2023-02-21 16:21:46 +0200

    Have the example update with values from outside

--- examples/dimmable_light/src/main.rs
+++ examples/dimmable_light/src/main.rs
@@ -18,8 +18,11 @@
 mod dev_att;
 use matter::core::{self, CommissioningData};
 use matter::data_model::cluster_basic_information::BasicInfoConfig;
-use matter::data_model::device_types::device_type_add_dimmable_light;
+use matter::data_model::cluster_level_control::{LevelControlCluster, UpdateDataStore};
+use matter::data_model::cluster_on_off::OnOffCluster;
+use matter::data_model::device_types::{device_type_add_dimmable_light, DEV_TYPE_DIMMABLE_LIGHT};
 use matter::secure_channel::spake2p::VerifierData;
+use std::{thread, time};
 
 fn main() {
     env_logger::init();
@@ -45,10 +48,29 @@ fn main() {
     let dm = matter.get_data_model();
     {
         let mut node = dm.node.write().unwrap();
-        let endpoint = device_type_add_dimmable_light(&mut node).unwrap();
+        let endpoint = node.add_endpoint(DEV_TYPE_DIMMABLE_LIGHT).unwrap();
+
+        node.add_cluster(endpoint, OnOffCluster::new().unwrap())
+            .unwrap();
+
+        let level_cluster = LevelControlCluster::new().unwrap();
+        let event_loop = level_cluster.as_ref().get_event_loop_ref().clone();
+        node.add_cluster(endpoint, level_cluster).unwrap();
+
+        // BG updater thread:
+        thread::spawn(move || {
+            println!("[bg thread] sleeping for 5 seconds.");
+            thread::sleep(time::Duration::from_secs(5));
+            println!(
+                "[bg thread] woke up. Pretend to process event from outside. Set Volume to 17."
+            );
+            let mut t = event_loop.lock().unwrap();
+            t.update_level(17);
+        });
+
         println!("Added OnOff Light Device type at endpoint id: {}", endpoint);
         println!("Data Model now is: {}", node);
-    }
+    };
 
     matter.start_daemon().unwrap();
 }
