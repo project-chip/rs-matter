0ed958fa17e468b4af43ce8e19bec6057cfc8720
ivmarkov <ivan.markov@gmail.com>
2024-02-12 14:11:38 +0000

    Leave a note w.r.t. stack size

--- examples/onoff_light/src/main.rs
+++ examples/onoff_light/src/main.rs
@@ -46,6 +46,13 @@ mod dev_att;
 
 fn main() -> Result<(), Error> {
     let thread = std::thread::Builder::new()
+        // Increase the stack size until the example can work without stack blowups.
+        // Note that the used stack size increases exponentially by lowering the level of compiler optimizations,
+        // as lower optimization settings prevent the Rust compiler from inlining constructor functions
+        // which often results in (unnecessary) memory moves and increased stack utilization:
+        // e.g., an opt-level of "0" will require a several times' larger stack.
+        //
+        // Optimizing/lowering `rs-matter` memory consumption is an ongoing topic.
         .stack_size(180 * 1024)
         .spawn(run)
         .unwrap();
