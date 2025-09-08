c813027b725764fec396d4dfbf45a99dd425cc47
William Hicklin <venturehicklin@gmail.com>
2025-09-08 10:54:31 +0000

    Added a nix shell config providing a standard Linux filesystem layout

---
+++ shell.nix
@@ -0,0 +1,46 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+
+(pkgs.buildFHSEnv {
+	name = "rs-matter-test-env";
+	runScript = "zsh";
+
+    targetPkgs = pkgs: with pkgs; [
+        # rust support
+        rustc
+        cargo
+        rustfmt
+        clippy
+
+        # Provide non-privileged execution of `ip` commands
+        iproute2
+        iptables
+
+        # connectedhomeip requirements
+        git
+        gcc
+        glib
+        glib.dev
+        pkg-config
+        cmake
+        ninja
+        gn
+        gobject-introspection.dev
+        gobject-introspection.out
+        openssl.dev
+        dbus.dev
+        avahi.dev
+        unzip
+        cairo.dev
+        readline.dev
+        jre
+        libffi.dev
+        zap-chip
+
+        ## python support
+        # Note: python 3.11 is required due to the deprecation of the `imp` module in newer versions of python.
+        python311Full
+        python311Packages.pip
+        python311Packages.virtualenv
+    ];
+}).env
