d59199b8337a0abc42f968765a8e46d4c7f57d91
William Hicklin <venturehicklin@gmail.com>
2025-09-10 14:46:44 +0100

    Implemented cluster init and on off state change coupling.

--- rs-matter/src/dm/clusters/level_control.rs
+++ rs-matter/src/dm/clusters/level_control.rs
@@ -31,6 +31,11 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
     const MAXIMUM_LEVEL: u8 = 254;
 
     // todo: add `on_off_state: Option<&'a OnOffState>` when OnOff in re-implemented.
+    // Creates a new instance of the LevelControlCluster.
+    //
+    // ## Panic
+    //
+    // panics if the `handler`'s `CLUSTER`` is misconfigured.
     pub fn new(dataver: Dataver,handler: &'a LevelControlState<'a, H>) -> Self {
         let cluster = Self {
             dataver,
@@ -41,6 +46,11 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
 
         cluster.validate();
 
+        // todo: call a pub(crate) method in OnOffCluster setting up `&cluster` as a LevelControlCluster
+        // accessor so it can call `coupled_on_off_cluster_on_off_state_change`
+
+        cluster.init();
+
         cluster
     }
 
@@ -48,7 +58,7 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
     //
     // ## Panic
     //
-    // panics with error message if the cluster setup is not valid according to the given cluster configuration.
+    // panics with error message if the `handler`'s `CLUSTER` is misconfigured.
     fn validate(&self) {
         if H::CLUSTER.revision != 5 {
             panic!("LevelControl validation: incorrect version number: expected 5 got {}", H::CLUSTER.revision);
@@ -123,6 +133,39 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
         }
     }
 
+    // Initialise the cluster on startup
+    fn init(&self) {
+        // 1.6.6.15. StartUpCurrentLevel Attribute
+        // This attribute SHALL indicate the desired startup level for a device when it is supplied with power
+        // and this level SHALL be reflected in the CurrentLevel attribute. The values of the
+        // StartUpCurrentLevel attribute are listed below:
+        // | Value        | Action on power up |
+        // |--------------| -------------------|
+        // | 0            | Set the CurrentLevel attribute to the minimum value permitted on the device |
+        // | null         | Set the CurrentLevel attribute to its previous value |
+        // | other values | Set the CurrentLevel attribute to this value |
+        // todo: Implement checking the reason for reboot.
+        // This behavior does not apply to reboots associated with OTA. After an OTA restart, the CurrentLevel
+        // attribute SHALL return to its value prior to the restart.
+
+        // `self.state` holds the previous current level as supplied by the SDK consumer.
+        // Hence, if this process errors, we quietly abort resulting in the previous current level.
+        if let Ok(startup_current_level) = self.state.start_up_current_level() {
+            if let Some(startup_current_level) = startup_current_level.into_option() {
+                // The spec fails to mention the need for this bounding.
+                let level = if startup_current_level < H::MIN_LEVEL {
+                    H::MIN_LEVEL
+                } else if startup_current_level > H::MAX_LEVEL {
+                    H::MAX_LEVEL
+                } else {
+                    startup_current_level
+                };
+
+                let _ = self.state.set_current_level(Maybe::some(level));
+            }
+        }
+    }
+    
     /// Adapt the handler instance to the generic `rs-matter` `Handler` trait
     pub const fn adapt(self) -> HandlerAsyncAdaptor<Self> {
         HandlerAsyncAdaptor(self)
@@ -196,6 +239,92 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
         }
     }
 
+    // This method is called by an OnOff cluster that is coupled with this LevelControl cluster.
+    // This method updates the CurrentLevel of the device when the state of the OnOff cluster changes.
+    //
+    // From section 1.6.4.1.1
+    // ## On
+    // Temporarily store CurrentLevel.
+    // Set CurrentLevel to the minimum level allowed
+    // for the device.
+    // Change CurrentLevel to OnLevel, or to the
+    // stored level if OnLevel is not defined, over the
+    // time period OnOffTransitionTime.
+    // ## off
+    // Temporarily store CurrentLevel.
+    // Change CurrentLevel to the minimum level
+    // allowed for the device over the time period
+    // OnOffTransitionTime.
+    // If OnLevel is not defined, set the CurrentLevel to
+    // the stored level.
+    pub(crate) fn coupled_on_off_cluster_on_off_state_change(&self, on: bool) -> Result<(), Error> {
+        self.task_signal.signal(Task::Stop);
+
+        let temp_current_level = match self.state.current_level()?.into_option() {
+            Some(current_level) => current_level,
+            None => return Err(ErrorCode::Failure.into()),
+        };
+
+        // use of unwrap is justified since this will option is always valid.
+        let bitmap = OptionsBitmap::from_bits(0).unwrap();
+
+        // 1.6.6.10. OnOffTransitionTime Attribute
+        // This attribute SHALL indicate the time taken to move to or from the target level when On or Off
+        // commands are received by an On/Off cluster on the same endpoint.
+        let mut transition_time = self.state.on_off_transition_time().unwrap_or(0);
+
+        match on {
+            true => {
+                let level = self.state.set_level(H::MIN_LEVEL).map_err(|_| ErrorCode::Failure)?;
+                self.state.write_current_level_quietly(Nullable::some(level), false)?;
+
+                let on_level = match self.state.on_level()?.into_option() {
+                    Some(on_level) => on_level,
+                    None => temp_current_level,
+                };
+
+                // 1.6.6.12. OnTransitionTime Attribute
+                // This attribute SHALL indicate the time taken to move the current level from the minimum level to
+                // the maximum level when an On command is received by an On/Off cluster on the same endpoint.
+                // If this attribute is not implemented, or contains a null value, the
+                // OnOffTransitionTime SHALL be used instead.
+                if let Ok(tt) = self.state.on_transition_time() {
+                    if let Some(tt) = tt.into_option() {
+                        // todo I'm unsure from the reading of the spec if this time should be proportional
+                        transition_time = tt
+                    }
+                };
+                
+                self.move_to_level(false, on_level, Some(transition_time), bitmap, bitmap)?;
+            },
+            false => {
+                // 1.6.6.13. OffTransitionTime Attribute
+                // This attribute SHALL indicate the time taken to move the current level from the maximum level to
+                // the minimum level when an Off command is received by an On/Off cluster on the same endpoint.
+                // If this attribute is not implemented, or contains a null value, the
+                // OnOffTransitionTime SHALL be used instead.
+                if let Ok(tt) = self.state.off_transition_time() {
+                    if let Some(tt) = tt.into_option() {
+                        // todo I'm unsure from the reading of the spec if this time should be proportional
+                        transition_time = tt
+                    }
+                };
+
+                if let Some(tt) = self.state.off_transition_time()?.into_option() {
+                    transition_time = tt
+                }
+
+                self.move_to_level(false, H::MIN_LEVEL, Some(transition_time), bitmap, bitmap)?;
+
+                if self.state.on_level()?.is_none() {
+                    self.state.set_current_level(Nullable::some(temp_current_level))?;
+                }
+            },
+        };
+
+        Ok(())
+    }
+
     // Update the on_off attribute of the OnOff cluster.
     // 
     // From section 1.6.4.1.2
@@ -229,7 +358,7 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
     }
 
     // A single move-to-level command handler for both with and without on off.
-    fn move_to_level(&self, with_on_off: bool, level: u8, transition_time: Option<u16>, options_mask: OptionsBitmap, options_override: OptionsBitmap) -> Result<(), Error> {
+    fn move_to_level(&self, with_on_off: bool,mut level: u8, transition_time: Option<u16>, options_mask: OptionsBitmap, options_override: OptionsBitmap) -> Result<(), Error> {
 
         if level > Self::MAXIMUM_LEVEL {
             return Err(ErrorCode::InvalidCommand.into())
@@ -239,13 +368,21 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
             return Ok(());
         }
 
+        if level > H::MAX_LEVEL {
+            level = H::MAX_LEVEL;
+            debug!("target level > MAX_LEVEL. level set to MAX_LEVEL")
+        }
+        else if level < H::MIN_LEVEL {
+            level = H::MIN_LEVEL;
+            debug!("target level < MIN_LEVEL. level set to MIN_LEVEL")
+        }
+
         info!("setting level to {} with transition time {:?}", level, transition_time);
 
         match transition_time {
             None | Some(0) => {
                 self.task_signal.signal(Task::Stop);
-                // todo find suitable error.
-                let level = self.state.set_level(level).map_err(|_| ErrorCode::Invalid)?;
+                let level = self.state.set_level(level).map_err(|_| ErrorCode::Failure)?;
                 self.state.write_current_level_quietly(Nullable::some(level), true)?;
                 self.state.write_remaining_time_quietly(Duration::from_millis(0), true)?;
 
@@ -266,11 +403,9 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
         let event_start_time = Instant::now();
 
         // Check if current_level is null. If so, return error.
-        //  todo: currently returning an incorrect error. `Failure` is used in the cpp impl
-        //  Equivalent code in cpp impl: https://github.com/project-chip/connectedhomeip/blob/8adaf97c152e478200784629499756e81c53fd15/src/app/clusters/level-control/level-control.cpp#L904
         let mut current_level = match self.state.current_level()?.into_option() {
             Some(cl) => cl,
-            None => return Err(ErrorCode::InvalidState.into()),
+            None => return Err(ErrorCode::Failure.into()),
         };
 
         let increasing = current_level < target_level;
@@ -293,8 +428,7 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
             let is_transition_end = current_level == target_level;
 
             debug!("move_to_level_transition: Setting current level: {}", current_level);
-            // todo find suitable error.
-            let current_level = self.state.set_level(current_level).map_err(|_| ErrorCode::Invalid)?;
+            let current_level = self.state.set_level(current_level).map_err(|_| ErrorCode::Failure)?;
             self.state.write_current_level_quietly(Nullable::some(current_level), is_transition_end)?;
 
 
@@ -407,8 +541,7 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
             }
 
             let is_end_of_transition = (new_level == H::MAX_LEVEL) || (new_level == H::MIN_LEVEL);
-            // todo find suitable error.
-            let new_level = self.state.set_level(new_level).map_err(|_| ErrorCode::Invalid)?;
+            let new_level = self.state.set_level(new_level).map_err(|_| ErrorCode::Failure)?;
             self.state.write_current_level_quietly(Maybe::some(new_level), is_end_of_transition)?;
 
             if is_end_of_transition {
@@ -465,8 +598,7 @@ impl<'a, H: LevelControlHooks> LevelControlCluster<'a, H> {
         match transition_time {
             0 => {
                 self.task_signal.signal(Task::Stop);
-                // todo find suitable error.
-                let new_level = self.state.set_level(new_level).map_err(|_| ErrorCode::Invalid)?;
+                let new_level = self.state.set_level(new_level).map_err(|_| ErrorCode::Failure)?;
                 self.state.write_current_level_quietly(Nullable::some(new_level), true)?;
                 self.state.write_remaining_time_quietly(Duration::from_millis(0), true)?;
 
@@ -537,8 +669,7 @@ impl<'a, H: LevelControlHooks> ClusterAsyncHandler for LevelControlCluster<'a, H
     ) -> Result<(), Error> {
         if let Some(level) = value.clone().into_option() {
             if level > H::MAX_LEVEL || level < H::MIN_LEVEL {
-                // todo not sure if this is the correct error
-                return Err(ErrorCode::InvalidData.into())
+                return Err(ErrorCode::ConstraintError.into())
             }
         }
 
@@ -633,10 +764,11 @@ impl<'a, H: LevelControlHooks> ClusterAsyncHandler for LevelControlCluster<'a, H
     }
 
     async fn set_start_up_current_level(&self, ctx: impl WriteContext, value:Nullable<u8>) -> Result<(), Error> {
+        // According to the current spec, this attribute dose not have any constraints at this stage.
+        // However, it's usage is bounded by min/max hence it makes sense to restrict the settable values to this range.
         if let Some(level) = value.clone().into_option() {
             if level > H::MAX_LEVEL || level < H::MIN_LEVEL {
-                // todo not sure if this is the correct error
-                return Err(ErrorCode::InvalidData.into())
+                return Err(ErrorCode::ConstraintError.into())
             }
         }
 
@@ -824,12 +956,13 @@ pub trait LevelControlHooks {
     const FASTEST_RATE: u8;
     const CLUSTER: Cluster<'static>;
 
-    // Implements the business logic for setting the level.
-    // DO NOT update attribute states.
-    // Returns the value to which the device was set.
+    // Implements the business logic for setting the level of the device.
+    // Returns the new level of the device.
+    // If this method errors, the `LevelControlCluster` will represent this with an `ImStatusCode` of `Failure`.
     //
-    // todo Should this be allowed to error? 
-    // If so, the error could be `()` and the SDK maps it to a suitable error signifying that something went wrong with the device.
+    // ## Implementation notes
+    //
+    // - DO NOT update attribute states, this is handled by the `LevelControlCluster`.
     fn set_level(&self, level: u8) -> Result<u8, ()>;
 
     // Raw accessors
